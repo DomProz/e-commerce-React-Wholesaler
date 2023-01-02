d3ddb808b5f3a37d1dd8e9e9a15b5222c6814834
Dominik Prozorowicz <d.prozorowicz@student.po.edu.pl>
2023-01-02 19:36:39 +0100

    ADD models,routes for Cart,User,Order,Product

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Success"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running");
+});
