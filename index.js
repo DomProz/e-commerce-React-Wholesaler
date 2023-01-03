2cb88fb66892ed7b6a7f7ce03f45bea9aae465ea
Dominik Prozorowicz <d.prozorowicz@student.po.edu.pl>
2023-01-03 02:09:23 +0100

    ADD routes for user,product

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const mongoose = require("mongoose");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL)
@@ -15,6 +17,7 @@ mongoose
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running");
